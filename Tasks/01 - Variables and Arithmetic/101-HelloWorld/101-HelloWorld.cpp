e443dac0d7b839e6bdc21a3c2e8adb4bb44199da
Archie Bullivant <doughton.archie@gmail.com>
2024-05-15 18:44:44 +0100

    Update 101-HelloWorld.cpp

--- Tasks/01 - Variables and Arithmetic/101-HelloWorld/101-HelloWorld.cpp
+++ Tasks/01 - Variables and Arithmetic/101-HelloWorld/101-HelloWorld.cpp
@@ -8,7 +8,9 @@
 int main()
 {
     //Write Hello World to the terminal with puts  ("put string")
+    puts("***********");
     puts("Hello World");
+    puts("***********");
 
     /*
         TASK: Add two more lines of code to the above so that the output reads as follows:
